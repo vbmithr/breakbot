f570190a550c02264318f4e7afd3c9513969b4b2
Vincent Bernardoff <vbmithr@gmail.com>
2012-09-25 10:27:11 +0200

    Made the main program more generic

--- breakbot.ml
+++ breakbot.ml
@@ -2,8 +2,6 @@ open Utils
 open Common
 open Exchange
 
-open Intersango
-
 (* Works only if processing is much faster than the rate at which we
    receive/parse the messages from the exchange. Otherwise processing
    might block the exchanges from updating... It has thus to be the
@@ -14,16 +12,15 @@ let () =
   Sys.catch_break true;
   (* Most performant method*)
   (* Lwt_unix.set_default_async_method Lwt_unix.Async_switch; *)
-  let intersango = new Intersango.intersango in
-  let mtgox = new Mtgox.mtgox in
-  let mvars = [intersango#get_mvar; mtgox#get_mvar] in
+  let exchanges = [new Intersango.intersango; new Mtgox.mtgox] in
+  let mvars = List.map (fun xch -> xch#get_mvar) exchanges in
   let rec process mvars =
     lwt xch = Lwt.pick (List.map Lwt_mvar.take mvars) in
     lwt () = Lwt_io.printf "Exchange %s has just been updated!\n" xch in
     process mvars in
   try
-    let threads_to_run = [mtgox#update (); intersango#update (); process mvars] in
+    let threads_to_run =
+      process mvars :: List.map (fun xch -> xch#update ()) exchanges in
     Lwt.join threads_to_run |> Lwt_main.run
   with Sys.Break ->
-    Printf.printf "Intersango:\n\n";
-    intersango#print;
+    List.iter (fun xch -> xch#print) exchanges
