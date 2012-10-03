73be4a2ad12e69258d0aa957ebc3bc96267b3e8c
Vincent Bernardoff <vbmithr@gmail.com>
2012-10-03 19:32:17 +0200

    Added a module to get the last exchange rates from the ECB

---
+++ ecb.ml
@@ -0,0 +1,24 @@
+(** Get the latest ECB rates *)
+
+open Utils
+
+open Cohttp_lwt_unix
+
+let url = "http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml"
+
+let get_rates ?(url=url) () =
+  let uri = Uri.of_string url in
+  lwt ret = Client.get uri in
+  let resp, body = Opt.unopt ret in
+  lwt xml = Body.string_of_body body in
+  let xml_input = Xmlm.make_input (`String (0, xml)) in
+  let rec parse acc input =
+    let next = try Some (Xmlm.input input) with _ -> None in
+    if next = None then acc else
+      match Opt.unopt next with
+        | `El_start ((_, "Cube"),
+                     [((_, "currency"), curr); ((_, "rate"), rate)]) ->
+          parse ((curr, float_of_string rate)::acc) input
+        | _ -> parse acc input
+  in
+  Lwt.return (parse [] xml_input)
