5f70d767394a917df2f35f664ebff1522b66e476
Vincent Bernardoff <vb@luminar.eu.org>
2012-11-25 11:42:02 +0100

    Removed unneeded dependencies

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,6 +1,6 @@
 open Ocamlbuild_plugin
 
 let () = dispatch begin function
-  | After_rules -> flag ["ocaml"; "compile";] & S[A"-ppopt"; A"-lwt-debug"]
+  | After_rules -> flag ["ocaml"; "compile"; "debug"] & S[A"-ppopt"; A"-lwt-debug"]
   | _ -> ()
 end
